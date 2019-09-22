1c4ed198da9f84660010b3245da206b9ddb44c75
Dejan Antic <dex.antic@gmail.com>
2019-09-22 18:30:26 +0200

    Refactured the button functionality

--- timer.js
+++ timer.js
@@ -1,7 +1,44 @@
-const startValue = 2700;
-let duration = startValue;
-const display = document.querySelector('.display');
-const startBtn = document.querySelector('#start');
-const pauseBtn = document.querySelector('#pause');
-const resetBtn = document.querySelector('#reset');
+(function Timer() {
+    const startValue = 2700;
+    let duration = startValue;
+    const timer = document.querySelector('.timer');
+    const display = document.querySelector('.display');
+    const startBtn = document.querySelector('#start');
+    const pauseBtn = document.querySelector('#pause');
+    const resetBtn = document.querySelector('#reset');
 
+
+    timer.addEventListener('click', function startTimer(e) {
+        if (e.target.tagName !== 'BUTTON') return;
+        let buttons = document.querySelectorAll('button');
+        switch (e.target.id) {
+            case 'start':
+                timer.dataset.status = 'active';
+                duration === startValue ? buttons.forEach(button => button.hidden = !button.hidden) : invertPlayPauseVisibility();
+                console.log('timer started'); // call timer() function on this line
+                duration--;
+                break;
+            case 'pause':
+                timer.dataset.status = 'paused';
+                invertPlayPauseVisibility();
+                console.log('timer paused');
+                break;
+            default:
+                timer.dataset.status = 'ready'
+                duration = startValue;
+                resetBtn.hidden = !resetBtn.hidden;
+                if (startBtn.hidden) invertPlayPauseVisibility();
+                console.log('timer was reset')
+        }
+    })
+
+    let invertPlayPauseVisibility = function switchButtonVisibility() {
+        startBtn.hidden = !startBtn.hidden;
+        pauseBtn.hidden = !pauseBtn.hidden;
+    }
+
+    let timer = function countdownTimer() {
+
+    }
+
+})();
